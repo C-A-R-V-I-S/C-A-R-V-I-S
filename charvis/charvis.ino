11347054edb75be4b502cf9f38c5e4a43f58fe3d
leejoochan <dsm2021@dsm.hs.kr>
2022-10-17 18:29:05 +0900

    remove_delay

--- charvis/charvis.ino
+++ charvis/charvis.ino
@@ -34,52 +34,52 @@ void moveFront(float F_Sonic,float R_Sonic,float fl_IR,float fr_IR,float rl_IR,f
   if(F_Sonic < 30){
     analogWrite(FL_1, 10);
     analogWrite(FR_1, 10); 
-    delay(50);
+    //delay(50);
 
     if(fl_IR < 10 && fr_IR < 10) {
       analogWrite(FL_1, 0); // 감속
       analogWrite(FR_1, 0);
-      delay(10);
+      //delay(10);
       analogWrite(FL_2, (F_Sonic * 15)); // 후진
       analogWrite(FR_2, 100);
-      delay(100);
+      //delay(100);
       analogWrite(FR_1, 250); // 좌회전
       analogWrite(RR_1, 40);
       analogWrite(FL_1, 50);
-      delay(300);
+      //delay(300);
       analogWrite(FL_1, 250); // 우회전
       analogWrite(RL_1, 40);
       analogWrite(FR_1, 50);
-      delay(300);
+      //delay(300);
     }
     else if(fl_IR < 10) {
       analogWrite(FR_1, 250); // 우회전                     
       analogWrite(RR_1, 40);
       analogWrite(FL_1, 50);
-      delay(300);
+      //delay(300);
     }
     else if(fr_IR < 10) {
       analogWrite(FR_1, 250); // 좌회전
       analogWrite(RR_1, 40);
       analogWrite(FL_1, 50);
-      delay(300);
+      //delay(300);
     }
     //웬만해서는 쓰이지 않겠지만 후진의 경우
     else if(R_Sonic < 30) {
       analogWrite(FL_2, 10); // 감속
       analogWrite(FR_2, 10);
-      delay(150);
+      //delay(150);
       if(rl_IR < 10) {
         analogWrite(RL_2, 250); // 우회전
         analogWrite(RR_2, 40);
         analogWrite(FL_2, 50);
-        delay(300);
+        //delay(300);
       }
       else if(rr_IR < 10) {
         analogWrite(RR_2, 250); // 좌회전
         analogWrite(RL_2, 40);
         analogWrite(FR_2, 50);
-        delay(300); 
+        //delay(300); 
       }
     }
   }
