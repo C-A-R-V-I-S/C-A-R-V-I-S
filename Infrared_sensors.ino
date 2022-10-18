dfd45addb53b25d8df3afb40ca00b98b73efbf11
김태완 <80623107+Joort@users.noreply.github.com>
2022-10-18 15:53:10 +0900

    Update Infrared_sensors.ino

--- Infrared_sensors.ino
+++ Infrared_sensors.ino
@@ -3,11 +3,18 @@
 #define Back_Left_Ir   28      //왼쪽 뒤 IR센서 28핀 설정
 #define Back_Right_Ir  29      //오른쪽 뒤 IR센서 29핀 설정
 
-int F_L_IR()                   //왼쪽 앞 IR센서의 측정값 저장하는 함수
+float F_L,F_R,B_L,B_R = 0;
+//F_L : 왼쪽 앞 IR센서 측정값 받는 변수
+//F_R : 오른쪽 앞 IR센서 측정값 받는 
+//B_L : 왼쪽 뒤 IR센서 측정값 받는 
+//B_R : 오른쪽 뒤 IR센서 측정값 받는 
+
+
+float F_L_IR()                   //왼쪽 앞 IR센서의 측정값 저장하는 함수
 {
-  int Front_Left_distance = 0;  //IR센서의 거리 저장 변수 선언 
-  int Front_Left_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
-  int Front_Left_volt = 0;      //IR센서의 측정값을 다듬을 변수 선언
+  float Front_Left_distance = 0;  //IR센서의 거리 저장 변수 선언 
+  float Front_Left_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
+  float Front_Left_volt = 0;      //IR센서의 측정값을 다듬을 변수 선언
 
   //적외선 거리 센서로 부터 측정값을 읽어온다
   Front_Left_data = analogRead(Front_Left_Ir);                   
@@ -16,14 +23,14 @@ int F_L_IR()                   //왼쪽 앞 IR센서의 측정값 저장하는 �
   //측정값을 통해 거리를 계산
   Front_Left_distance = (21.61/(Front_Left_volt-0.1696))*1000;   
   //delay(500);                                                  
-  return Front_Left_distance;                              
+  F_L = Front_Left_distance;                              
 }
 
-int F_R_IR()                   //오른쪽 앞 IR센서의 측정값 저장하는 함수       
+float F_R_IR()                   //오른쪽 앞 IR센서의 측정값 저장하는 함수       
 {
-  int Front_Right_distance = 0;  //IR센서의 거리 저장 변수 선언 
-  int Front_Right_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
-  int Front_Right_volt = 0;      //IR센서의 측정값을 다듬을 변수 선언
+  float Front_Right_distance = 0;  //IR센서의 거리 저장 변수 선언 
+  float Front_Right_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
+  float Front_Right_volt = 0;      //IR센서의 측정값을 다듬을 변수 선언
 
   //적외선 거리 센서로 부터 측정값을 읽어온다
   Front_Right_data = analogRead(Front_Right_Ir);                 
@@ -32,14 +39,14 @@ int F_R_IR()                   //오른쪽 앞 IR센서의 측정값 저장하�
   //측정값을 통해 거리를 계산  
   Front_Right_distance = (21.61/(Front_Right_volt-0.1696))*1000; 
   //delay(500);                                                  
-  return Front_Right_distance;                                    
+  F_R = Front_Right_distance;                                    
 }
 
-int B_L_IR()                   //왼쪽 뒤 IR센서의 측정값 저장하는 함수        
+float B_L_IR()                   //왼쪽 뒤 IR센서의 측정값 저장하는 함수        
 {
-  int Back_Left_distance = 0;    //IR센서의 거리 저장 변수 선언 
-  int Back_Left_data = 0;        //IR센서로 부터 측정값 읽는 변수 선언
-  int Back_Left_volt = 0;        //IR센서의 측정값을 다듬을 변수 선언
+  float Back_Left_distance = 0;    //IR센서의 거리 저장 변수 선언 
+  float Back_Left_data = 0;        //IR센서로 부터 측정값 읽는 변수 선언
+  float Back_Left_volt = 0;        //IR센서의 측정값을 다듬을 변수 선언
 
   //적외선 거리 센서로 부터 측정값을 읽어온다
   Back_Left_data = analogRead(Back_Left_Ir);                     
@@ -48,14 +55,14 @@ int B_L_IR()                   //왼쪽 뒤 IR센서의 측정값 저장하는 �
   //측정값을 통해 거리를 계산
   Back_Left_distance = (21.61/(Back_Left_volt-0.1696))*1000;     
   //delay(500);                                                  
-  return Back_Left_distance;                                    
+  B_L = Back_Left_distance;                                    
 }
 
-int B_R_IR()                   //오른쪽 뒤 IR센서의 측정값 저장하는 함수       
+float B_R_IR()                   //오른쪽 뒤 IR센서의 측정값 저장하는 함수       
 {
-  int Back_Right_distance = 0;   //IR센서의 거리 저장 변수 선언 
-  int Back_Right_data = 0;       //IR센서로 부터 측정값 읽는 변수 선언
-  int Back_Right_volt = 0;       //IR센서의 측정값을 다듬을 변수 선언
+  float Back_Right_distance = 0;   //IR센서의 거리 저장 변수 선언 
+  float Back_Right_data = 0;       //IR센서로 부터 측정값 읽는 변수 선언
+  float Back_Right_volt = 0;       //IR센서의 측정값을 다듬을 변수 선언
 
   //적외선 거리 센서로 부터 측정값을 읽어온다
   Back_Right_data = analogRead(Back_Right_Ir);                   
@@ -64,5 +71,5 @@ int B_R_IR()                   //오른쪽 뒤 IR센서의 측정값 저장하�
   //측정값을 통해 거리를 계산
   Back_Right_distance = (21.61/(Back_Right_volt-0.1696))*1000;   
   //delay(500);                                                  
-  return Back_Right_distance;                                    
+  B_R = Back_Right_distance;                                    
 }
