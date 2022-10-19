e0d4e3785b6a14e7cc51e165cf4eaa51499059bd
leejoochan <dsm2021@dsm.hs.kr>
2022-10-19 11:55:48 +0900

    fix motors moving

--- charvis/charvis.ino
+++ charvis/charvis.ino
@@ -2,85 +2,80 @@
 #define FL_2 3
 #define FR_1 4
 #define FR_2 5
-#define RL_1 6
-#define RL_2 7
-#define RR_1 8
-#define RR_2 9
+#define BL_1 6
+#define BL_2 7
+#define BR_1 8
+#define BR_2 9
 
 // 구동 바퀴 위치
-// 첫 번째의 F(front)와 R(rear)은 앞바퀴, 뒷바퀴
+// 첫 번째의 F(front)와 B(back)은 앞바퀴, 뒷바퀴
 // 두 번째의 L(left)와 R(right)은 왼쪽바퀴, 오른쪽바퀴 
 
 // 회전 방향
 // 세 번째의 1은 전진, 2는 후진
 
 
-void setup() { // 모터제어에 필요한 핀 8개를 OUTPUT으로 설정
+void setup() {
   for(int i=2;i<10;i++){
     pinMode(i, OUTPUT);
   }
 }
 
-// 기본적인 전진 구동을 위해 moveFront()함수를 실행
 void loop() {
-  moveFront();
+  
 }
 
 
-void moveFront(float F_Sonic,float R_Sonic,float fl_IR,float fr_IR,float rl_IR,float rr_IR) {
-  analogWrite(FL_1, 200);
-  analogWrite(FR_1, 200);
-
-  if(F_Sonic < 30){
-    analogWrite(FL_1, 10);
-    analogWrite(FR_1, 10); 
-    //delay(50);
-
-    if(fl_IR < 10 && fr_IR < 10) {
-      analogWrite(FL_1, 0); // 감속
-      analogWrite(FR_1, 0);
-      //delay(10);
-      analogWrite(FL_2, (F_Sonic * 15)); // 후진
-      analogWrite(FR_2, 100);
-      //delay(100);
-      analogWrite(FR_1, 250); // 좌회전
-      analogWrite(RR_1, 40);
-      analogWrite(FL_1, 50);
-      //delay(300);
-      analogWrite(FL_1, 250); // 우회전
-      analogWrite(RL_1, 40);
-      analogWrite(FR_1, 50);
-      //delay(300);
-    }
-    else if(fl_IR < 10) {
-      analogWrite(FR_1, 250); // 우회전                     
-      analogWrite(RR_1, 40);
-      analogWrite(FL_1, 50);
-      //delay(300);
-    }
-    else if(fr_IR < 10) {
-      analogWrite(FR_1, 250); // 좌회전
-      analogWrite(RR_1, 40);
-      analogWrite(FL_1, 50);
-      //delay(300);
-    }
-    //웬만해서는 쓰이지 않겠지만 후진의 경우
-    else if(R_Sonic < 30) {
-      analogWrite(FL_2, 10); // 감속
-      analogWrite(FR_2, 10);
-      //delay(150);
-      if(rl_IR < 10) {
-        analogWrite(RL_2, 250); // 우회전
-        analogWrite(RR_2, 40);
-        analogWrite(FL_2, 50);
-        //delay(300);
-      }
-      else if(rr_IR < 10) {
-        analogWrite(RR_2, 250); // 좌회전
-        analogWrite(RL_2, 40);
-        analogWrite(FR_2, 50);
-        //delay(300); 
-      }
-    }
+void moveFront(int spd, int t) {
+  //spd는 0~255의 값으로 모터 속력을 의미
+  //t는 time의 약어로 몇ms 동안 이동할지를 의미
+  unsigned char prev_m = 0;
+  unsigned char curr_m = 0;
+  curr_m = millis();
+  if(curr_m - prev_m == 1000){
+    return;
   }
+  analogWrite(FL_1, spd);
+  analogWrite(FR_1, spd);
+}
+
+void moveLeft(int spd, int t) {
+  //spd는 0~255의 값으로 모터 속력을 의미
+  //t는 time의 약어로 몇ms 동안 이동할지를 의미
+  unsigned char prev_m = 0;
+  unsigned char curr_m = 0;
+  curr_m = millis();
+  if(curr_m - prev_m == 1000){
+    return;
+  }
+  analogWrite(FL_1, spd/10);
+  analogWrite(BR_1, spd/10);
+  analogWrite(FR_1, spd);
+}
+
+void moveright(int spd, int t) {
+  //spd는 0~255의 값으로 모터 속력을 의미
+  //t는 time의 약어로 몇ms 동안 이동할지를 의미
+  unsigned char prev_m = 0;
+  unsigned char curr_m = 0;
+  curr_m = millis();
+  if(curr_m - prev_m == 1000){
+    return;
+  }
+  analogWrite(FR_1, spd/10);
+  analogWrite(BL_1, spd/10);
+  analogWrite(FL_1, spd);
+}
+
+void moveBack(int spd, int t) {
+  //spd는 0~255의 값으로 모터 속력을 의미
+  //t는 time의 약어로 몇ms 동안 이동할지를 의미
+  unsigned char prev_m = 0;
+  unsigned char curr_m = 0;
+  curr_m = millis();
+  if(curr_m - prev_m == 1000){
+    return;
+  }
+  analogWrite(FL_2, spd);
+  analogWrite(FR_2, spd);
 }
