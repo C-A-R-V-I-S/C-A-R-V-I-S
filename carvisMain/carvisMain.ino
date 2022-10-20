66052482484ec7f152439db2115ebd70bd8f78b8
ParkJongMin0805 <wkdrns805@gmail.com>
2022-10-20 12:05:59 +0900

    retouch main(2)

--- carvisMain/carvisMain.ino
+++ carvisMain/carvisMain.ino
@@ -7,6 +7,11 @@
 #define BR_1 8
 #define BR_2 9
 
+#define LED_FL 22
+#define LED_FR 23
+#define LED_BL 24
+#define LED_BR 25
+
 #define Front_Left_Ir  26      //왼쪽 앞 IR센서 26핀 설정
 #define Front_Right_Ir 27    //오른쪽 앞 IR센서 27핀 설정
 #define Back_Left_Ir   28      //왼쪽 뒤 IR센서 28핀 설정
@@ -147,56 +152,68 @@ float B_R_IR()                   //오른쪽 뒤 IR센서의 측정값 저장하
 // 회전 방향
 // 세 번째의 1은 전진, 2는 후진
 
-void moveFront(int spd, int t) {
+void moveFront(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
-  //t는 time의 약어로 몇ms 동안 이동할지를 의미
+  //t는 time의 약어로 몇초 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_FL, HIGH);
+  digitalWrite(LED_FR, HIGH);
+  
   analogWrite(FL_1, spd);
   analogWrite(FR_1, spd);
 }
 
-void moveLeft(int spd, int t) {
+void moveLeft(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
   //t는 time의 약어로 몇ms 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_FL, HIGH);
+  digitalWrite(LED_BL, HIGH);
+  
   analogWrite(FL_1, spd/10);
   analogWrite(BR_1, spd/10);
   analogWrite(FR_1, spd);
 }
 
-void moveright(int spd, int t) {
+void moveright(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
   //t는 time의 약어로 몇ms 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_FR, HIGH);
+  digitalWrite(LED_BR, HIGH);
+  
   analogWrite(FR_1, spd/10);
   analogWrite(BL_1, spd/10);
   analogWrite(FL_1, spd);
 }
 
-void moveBack(int spd, int t) {
+void moveBack(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
   //t는 time의 약어로 몇ms 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_BL, HIGH);
+  digitalWrite(LED_BR, HIGH);
+  
   analogWrite(FL_2, spd);
   analogWrite(FR_2, spd);
 }
@@ -241,14 +258,17 @@ void setup() {
   for(int i=2;i<10;i++){
     pinMode(i, OUTPUT);
   }
-  for(int i=26;i<30;i++){
+  for(int i=22;i<26;i++){
     pinMode(i, OUTPUT);
+  }
+  for(int i=26;i<30;i++){
+    pinMode(i, INPUT);
   }
   pinMode(Front_Trig,INPUT);
   pinMode(Front_Echo, OUTPUT);
   pinMode(Back_Trig,INPUT); 
   pinMode(Back_Echo, OUTPUT);
-  Serial.println("HELLO!"); 
+  Serial.println("CARVIS START!"); 
 }
 
 void loop() {
