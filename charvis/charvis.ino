672febdda433acbe8895569a159cb69a948cfb04
leejoochan <dsm2021@dsm.hs.kr>
2022-10-20 10:33:19 +0900

    fix : led작동을 추가 및 함수의 매개변수 타입 수정

--- charvis/charvis.ino
+++ charvis/charvis.ino
@@ -7,6 +7,11 @@
 #define BR_1 8
 #define BR_2 9
 
+#define LED_FL 22
+#define LED_FR 23
+#define LED_BL 24
+#define LED_BR 25
+
 // 구동 바퀴 위치
 // 첫 번째의 F(front)와 B(back)은 앞바퀴, 뒷바퀴
 // 두 번째의 L(left)와 R(right)은 왼쪽바퀴, 오른쪽바퀴 
@@ -26,56 +31,68 @@ void loop() {
 }
 
 
-void moveFront(int spd, int t) {
+void moveFront(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
-  //t는 time의 약어로 몇ms 동안 이동할지를 의미
+  //t는 time의 약어로 몇초 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_FL, HIGH);
+  digitalWrite(LED_FR, HIGH);
+  
   analogWrite(FL_1, spd);
   analogWrite(FR_1, spd);
 }
 
-void moveLeft(int spd, int t) {
+void moveLeft(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
   //t는 time의 약어로 몇ms 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_FL, HIGH);
+  digitalWrite(LED_BL, HIGH);
+  
   analogWrite(FL_1, spd/10);
   analogWrite(BR_1, spd/10);
   analogWrite(FR_1, spd);
 }
 
-void moveright(int spd, int t) {
+void moveright(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
   //t는 time의 약어로 몇ms 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_FR, HIGH);
+  digitalWrite(LED_BR, HIGH);
+  
   analogWrite(FR_1, spd/10);
   analogWrite(BL_1, spd/10);
   analogWrite(FL_1, spd);
 }
 
-void moveBack(int spd, int t) {
+void moveBack(int spd, float t) {
   //spd는 0~255의 값으로 모터 속력을 의미
   //t는 time의 약어로 몇ms 동안 이동할지를 의미
   unsigned char prev_m = 0;
   unsigned char curr_m = 0;
   curr_m = millis();
-  if(curr_m - prev_m == 1000){
+  if(curr_m - prev_m == t*1000){
     return;
   }
+  digitalWrite(LED_BL, HIGH);
+  digitalWrite(LED_BR, HIGH);
+  
   analogWrite(FL_2, spd);
   analogWrite(FR_2, spd);
 }
