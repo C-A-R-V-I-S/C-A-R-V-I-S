e8ecc2fbffd9fb6fb7596edc24fb38d054ca27b2
김태완 <80623107+Joort@users.noreply.github.com>
2022-10-26 16:51:06 +0900

    계산 식 수정 및 코드 최소화
    부품 판매 사이트가 제공한 거리구하는 예제를 가져와 사용했기에 
    거리가 정상출력 되지않는다면 코드 특성상 측정전압에 따른 계산을 하기 때문에
    전압에 문제가 있을 수 있습니다.

--- Infrared_sensors/Infrared_sensors.ino
+++ Infrared_sensors/Infrared_sensors.ino
@@ -2,6 +2,7 @@
 #define Front_Right_Ir 27    //오른쪽 앞 IR센서 27핀 설정
 #define Back_Left_Ir   28      //왼쪽 뒤 IR센서 28핀 설정
 #define Back_Right_Ir  29      //오른쪽 뒤 IR센서 29핀 설정
+#define VOLTS_PER_UNIT .0049F //아날로그 값을 volt 단위로 변환하기위한 변수 설정
 
 float F_L,F_R,B_L,B_R = 0;
 //F_L : 왼쪽 앞 IR센서 측정값 받는 변수
@@ -12,42 +13,44 @@ float F_L,F_R,B_L,B_R = 0;
 
 void F_L_IR()                   //왼쪽 앞 IR센서의 측정값 저장하는 함수
 {
-  float Front_Left_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
-
-  //적외선 거리 센서로 부터 측정값을 읽어온다
-  Front_Left_data = analogRead(Front_Left_Ir);
-  //측정값을 통해 거리를 계산                   
-
-  F_L = 10650.08 * pow(Front_Left_data,-0.935) - 10;  //단위는 cm
+  //IR센서로 부터 측정값 읽는 변수 선언
+  //적외선 거리 센서로 부터 측정값을 읽어온다   
+  int Front_Left_data = analogRead(Front_Left_Ir);
+  // 아날로그값을 volt 단위로 변환
+  float volts = (float)Front_Left_data * VOLTS_PER_UNIT;
+  // 측정전압에 따른 cm단위 거리 계산
+  F_L = 60.495 * pow(volts,-1.1904);  
 }
 
 void F_R_IR()                   //왼쪽 앞 IR센서의 측정값 저장하는 함수
 {
-  float Front_Right_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
-
-  //적외선 거리 센서로 부터 측정값을 읽어온다
-  Front_Right_data = analogRead(Front_Right_Ir);
-  //측정값을 통해 거리를 계산                   
-  F_R = 10650.08 * pow(Front_Right_data,-0.935) - 10;  //단위는 cm
+  //IR센서로 부터 측정값 읽는 변수 선언
+  //적외선 거리 센서로 부터 측정값을 읽어온다   
+  int Front_Right_data = analogRead(Front_Right_Ir);
+  // 아날로그값을 volt 단위로 변환
+  float volts = (float)Front_Right_data * VOLTS_PER_UNIT;
+  // 측정전압에 따른 cm단위 거리 계산
+  F_R = 60.495 * pow(volts,-1.1904);  
 }
 
 void B_L_IR()                   //왼쪽 앞 IR센서의 측정값 저장하는 함수
 {
-  float Back_Left_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
-
+  //IR센서로 부터 측정값 읽는 변수 선언
   //적외선 거리 센서로 부터 측정값을 읽어온다
-  Back_Left_data = analogRead(Back_Left_Ir);
-  //측정값을 통해 거리를 계산                   
-
-  B_L = 10650.08 * pow(Back_Left_data,-0.935) - 10;  //단위는 cm
+  int Back_Left_data = analogRead(Back_Left_Ir);
+  // 아날로그값을 volt 단위로 변환
+  float volts = (float)Back_Left_data * VOLTS_PER_UNIT;
+  // 측정전압에 따른 cm단위 거리 계산
+  B_L = 60.495 * pow(volts,-1.1904);  
 }
 
 void B_R_IR()                   //왼쪽 앞 IR센서의 측정값 저장하는 함수
 {
-  float Back_Right_data = 0;      //IR센서로 부터 측정값 읽는 변수 선언
-
-  //적외선 거리 센서로 부터 측정값을 읽어온다
-  Back_Right_data = analogRead(Back_Right_Ir);
-  //측정값을 통해 거리를 계산                   
-  B_R = 10650.08 * pow(Back_Right_data,-0.935) - 10;  //단위는 cm                                                                                                                                                         
+  //IR센서로 부터 측정값 읽는 변수 선언
+  //적외선 거리 센서로 부터 측정값을 읽어온다   
+  int Back_Right_data = analogRead(Back_Right_Ir);
+  // 아날로그값을 volt 단위로 변환
+  float volts = (float)Back_Right_data * VOLTS_PER_UNIT;
+  // 측정전압에 따른 cm단위 거리 계산
+  B_R = 60.495 * pow(volts,-1.1904);                                                                                                                                                    
 }
